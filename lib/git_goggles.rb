1089e783975d15617ff661b9b7a64534f28d7358
ben <bmills@benmillsdesigns.com>
2012-05-10 23:55:39 -0500

    Setup project.

---
+++ lib/git_goggles.rb
@@ -0,0 +1,7 @@
+require 'sinatra/base'
+require 'grit'
+
+require 'git_goggles/app'
+
+module GitGoggles
+end
---
+++ lib/git_goggles/app.rb
@@ -0,0 +1,7 @@
+module GitGoggles
+  class App < Sinatra::Base
+    get '/' do
+      'Hello world'
+    end
+  end
+end
